7baa10236c77635dd672b1eb4e53b9fbc2e7ce6a
Gerhard Gruber <sparhawk@gmx.at>
2025-06-08 15:48:46 +0200

    Renamed property to DockAnchor.

--- WpfDockManager/DockingPanel.cs
+++ WpfDockManager/DockingPanel.cs
@@ -40,7 +40,8 @@ namespace WpfDockManager
 		Left,
 		Top,
 		Right,
-		Bottom
+		Bottom,
+		Floating
 	}
 
 	public class DockingPanel : Panel
@@ -69,15 +70,17 @@ namespace WpfDockManager
 						),
 						new ValidateValueCallback(IsValidDock)
 				);
+
 		internal static bool IsValidDock(object o)
 		{
 			DockType dock = (DockType)o;
 
-			return (dock == DockType.Left
+			return (dock == DockType.None
+					|| dock == DockType.Left
 					|| dock == DockType.Top
 					|| dock == DockType.Right
 					|| dock == DockType.Bottom
-					|| dock == DockType.None
+					|| dock == DockType.Floating
 					);
 		}
 
@@ -110,6 +113,109 @@ namespace WpfDockManager
 			element.SetValue(DockProperty, value);
 		}
 		#endregion Dock property
+		#region DockAnchor property
+		public static readonly DependencyProperty DockAnchorProperty =
+				DependencyProperty.RegisterAttached(
+						"DockAnchor",
+						typeof(string),
+						typeof(DockingPanel),
+						new FrameworkPropertyMetadata(
+							"",
+							new PropertyChangedCallback(OnDockAnchorChanged)
+						)
+					);
+
+		private static void OnDockAnchorChanged(DependencyObject child, DependencyPropertyChangedEventArgs e)
+		{
+			var parent = DockingHelper.FindParentClass<DockingPanel>(child);
+			if (parent == null)
+				return;
+
+			var newGroup = (e.NewValue as string)!;
+			var tabCtrl = parent.FindAnchor(newGroup);
+
+			// If we already have a tab for this group we don't need to do anything.
+			if (tabCtrl == null)
+			{
+				if (newGroup.Length > 0)
+				{
+					tabCtrl = new TabControl();
+					parent.DockGroups[newGroup] = tabCtrl;
+					SetDockAnchor(tabCtrl, newGroup+"Anchor");
+
+					var tab = parent.GetRootTabControl();
+					if (tab == null)
+						parent.AttachToGrid(tabCtrl);
+				}
+			}
+
+			var oldGroup = (e.OldValue as string)!;
+			if (oldGroup.Length == 0)
+				return;
+
+			tabCtrl = parent.FindAnchor(oldGroup);
+			if (tabCtrl == null)
+				return;
+
+			parent.DockGroups.Remove(oldGroup);
+		}
+
+		public static string GetDockAnchor(UIElement element)
+		{
+			ArgumentNullException.ThrowIfNull(element);
+			return (string)element.GetValue(DockAnchorProperty);
+		}
+
+		public static void SetDockAnchor(UIElement element, string value)
+		{
+			ArgumentNullException.ThrowIfNull(element);
+			element.SetValue(DockAnchorProperty, value);
+		}
+		#endregion DockAnchor property
+		#region DockIndex property
+		public static readonly DependencyProperty DockIndexProperty =
+				DependencyProperty.RegisterAttached(
+						"DockIndex",
+						typeof(int),
+						typeof(DockingPanel),
+						new FrameworkPropertyMetadata(
+							-1
+							)
+					);
+		public static int GetDockIndex(UIElement element)
+		{
+			ArgumentNullException.ThrowIfNull(element);
+			return (int)element.GetValue(DockIndexProperty);
+		}
+
+		public static void SetDockIndex(UIElement element, int value)
+		{
+			ArgumentNullException.ThrowIfNull(element);
+			element.SetValue(DockIndexProperty, value);
+		}
+		#endregion DockIndex property
+		#region DockTarget property
+		public static readonly DependencyProperty DockTargetProperty =
+				DependencyProperty.RegisterAttached(
+						"DockTarget",
+						typeof(string),
+						typeof(DockingPanel),
+						new FrameworkPropertyMetadata(
+							""
+						)
+					);
+		public static string GetDockTarget(UIElement element)
+		{
+			ArgumentNullException.ThrowIfNull(element);
+			return (string)element.GetValue(DockTargetProperty);
+		}
+
+		public static void SetDockTarget(UIElement element, string value)
+		{
+			ArgumentNullException.ThrowIfNull(element);
+			element.SetValue(DockTargetProperty, value);
+		}
+		#endregion DockTarget property
 		#region DockTitle property
 		public static readonly DependencyProperty DockTitleProperty =
 				DependencyProperty.RegisterAttached(
@@ -144,83 +250,6 @@ namespace WpfDockManager
 			element.SetValue(DockTitleProperty, value);
 		}
 		#endregion DockTitle property
-		#region DockGroup property
-		public static readonly DependencyProperty DockGroupProperty =
-				DependencyProperty.RegisterAttached(
-						"DockGroup",
-						typeof(string),
-						typeof(DockingPanel),
-						new FrameworkPropertyMetadata(
-							"",
-							new PropertyChangedCallback(OnDockGroupChanged)
-						)
-					);
-
-		private static void OnDockGroupChanged(DependencyObject child, DependencyPropertyChangedEventArgs e)
-		{
-			var parent = DockingHelper.FindParentClass<DockingPanel>(child);
-			if (parent == null)
-				return;
-
-			var newGroup = (e.NewValue as string)!;
-			var tabCtrl = parent.FindGroup(newGroup);
-
-			// If we already have a tab for this group we don't need to do anything.
-			if (tabCtrl == null)
-			{
-				if (newGroup.Length > 0)
-				{
-					tabCtrl = new TabControl();
-					parent.DockGroups[newGroup] = tabCtrl;
-					SetDockGroup(tabCtrl, newGroup);
-				}
-			}
-
-			var oldGroup = (e.OldValue as string)!;
-			if (oldGroup.Length == 0)
-				return;
-
-			tabCtrl = parent.FindGroup(oldGroup);
-			if (tabCtrl == null)
-				return;
-
-			parent.DockGroups.Remove(oldGroup);
-		}
-
-		public static string GetDockGroup(UIElement element)
-		{
-			ArgumentNullException.ThrowIfNull(element);
-			return (string)element.GetValue(DockGroupProperty);
-		}
-
-		public static void SetDockGroup(UIElement element, string value)
-		{
-			ArgumentNullException.ThrowIfNull(element);
-			element.SetValue(DockGroupProperty, value);
-		}
-		#endregion DockGroup property
-		#region DockIndex property
-		public static readonly DependencyProperty DockIndexProperty =
-				DependencyProperty.RegisterAttached(
-						"DockIndex",
-						typeof(int),
-						typeof(DockingPanel),
-						new FrameworkPropertyMetadata(
-							-1
-							)
-					);
-		public static int GetDockIndex(UIElement element)
-		{
-			ArgumentNullException.ThrowIfNull(element);
-			return (int)element.GetValue(DockIndexProperty);
-		}
-
-		public static void SetDockIndex(UIElement element, int value)
-		{
-			ArgumentNullException.ThrowIfNull(element);
-			element.SetValue(DockIndexProperty, value);
-		}
-		#endregion DockIndex property
 
 		public DockingPanel()
 			: base()
@@ -243,7 +272,7 @@ namespace WpfDockManager
 			// We can no longer rely on the references anyway, as they might have been
 			// destroyed or moved, so we discard them.
 			LayoutItems = null;
-			DockGroups = new ();
+			//DockGroups = new ();
 		}
 
 		private Grid CreateDefaultGrid()
@@ -324,18 +353,17 @@ namespace WpfDockManager
 			//InvalidateMeasure();
 		}
 
-		private void ResetProperties(UIElement? element)
+		private static void ResetProperties(UIElement? element)
 		{
 			if (element == null)
 				return;
 
 			element.ClearValue(DockProperty);
-			element.ClearValue(DockGroupProperty);
+			element.ClearValue(DockAnchorProperty);
 			element.ClearValue(DockIndexProperty);
 		}
 
-		#region Un-/Docking of elements
-		private TabControl? FindGroup(string? group)
+		private TabControl? FindAnchor(string? group)
 		{
 			if (group == null || group.Length == 0)
 				return null;
@@ -373,7 +401,8 @@ namespace WpfDockManager
 				if (element == null)
 					continue;
 
-				TabControl? target = FindGroup(GetDockGroup(element));
+				var nm = GetDockAnchor(element);
+				TabControl? target = FindAnchor(nm);
 				if (target == null)
 					target = GetRootTabControl();
 
@@ -395,34 +424,47 @@ namespace WpfDockManager
 			return _rootGrid.Children[0] as TabControl;
 		}
 
+		public void AttachToGrid(UIElement element, Grid? grid = null, int row = 0, int column = 0)
+		{
+			if (grid == null)
+				grid = _rootGrid;
+
+			grid.Children.Add(element);
+			Grid.SetRow(element, row);
+			Grid.SetColumn(element, column);
+		}
+
 		/// <summary>
 		/// Wrap a GUI item in a tabcontrol. If the tabcontrol doesn't exist it will be created.
 		/// If index is not specified, it will be appended at the end.
 		/// </summary>
 		/// <param name="element"></param>
 		/// <returns>The specified tabctrl or a new one.</returns>
-		public TabControl CreateElementTab(FrameworkElement element, TabControl? tabCtrl = null, int index = -1, string? title = "")
+		protected static TabControl CreateElementTab(FrameworkElement element, TabControl? tabCtrl = null, int index = -1)
 		{
 			DockingHelper.RemoveElementFromItsParent(element);
 
 			if (tabCtrl == null)
 				tabCtrl = new TabControl();
 
+			var title = GetDockTitle(element);
 			if (title == null)
 				title = "";
 
-			TabItem ti = new TabItem();
-			ti.Header = title;
-			ti.Content = element;
+			var ti = new TabItem
+			{
+				Header = title,
+				Content = element
+			};
+
 			if (index == -1)
 				index = tabCtrl.Items.Count;
-
 			tabCtrl.Items.Insert(index, ti);
 
 			return tabCtrl;
 		}
 
-		public void DockElement(UIElement element, DockType dock, UIElement? target = null, int index = -1)
+		public void DockElement(UIElement element, DockType dock, UIElement? target = null, int index = -1, bool batchDock = false)
 		{
 			if (element == target)
 				throw new ArgumentException("Can not dock an element on itself!");
@@ -443,26 +485,53 @@ namespace WpfDockManager
 			{
 				case DockType.None:
 				{
-					var title = GetDockTitle(element);
+					var tabCtrl = CreateElementTab(item, tabCtrl: target as TabControl, index: index);
 
-					// If we have no target for a child which should be attached to the center
-					// we add it to the root.
-					var tabCtrl = CreateElementTab(item, tabCtrl: target as TabControl, title: title, index: index);
+					// If an element should be added to the center we need a target to add the item to.
+					// Only if the panel is empty, we can create a new tab automatically.
 					if (target == null)
 					{
-						_rootGrid.Children.Add(tabCtrl);
-						Grid.SetRow(tabCtrl, 0);
-						Grid.SetColumn(tabCtrl, 0);
-						target = tabCtrl;
+						if (GetRootTabControl() != null)
+							throw new InvalidOperationException("If no dock position is specified the element needs a target");
 
-						//throw new InvalidOperationException("Item can not be added without a target");
+						AttachToGrid(tabCtrl);
 					}
 				}
 				break;
+
+				case DockType.Left:
+				{
+				}
+				break;
+
+				case DockType.Right:
+				{
+				}
+				break;
+
+				case DockType.Top:
+				{
+				}
+				break;
+
+				case DockType.Bottom:
+				{
+				}
+				break;
+
+				case DockType.Floating:
+				{
+				}
+				break;
+
+				default:
+					throw new InvalidOperationException("Undefined docking position: "+dock.ToString());
 			}
 
-			InvalidateMeasure();
+			if (!batchDock)
+				InvalidateMeasure();
 		}
+
 		protected void UndockElement(UIElement? element)
 		{
 			if (element == null)
@@ -470,6 +539,5 @@ namespace WpfDockManager
 
 			InvalidateMeasure();
 		}
-		#endregion Un-/Docking of elements
 	}
 }
