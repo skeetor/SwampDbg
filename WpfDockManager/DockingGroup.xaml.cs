7da68a40975195a2d5a798e4fe8984adc9086ffb
Gerhard Gruber <sparhawk@gmx.at>
2025-06-17 19:29:07 +0200

    DockingGroup added

---
+++ WpfDockManager/DockingGroup.xaml.cs
@@ -0,0 +1,85 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace WpfDockManager
+{
+	public enum TabPosition
+	{
+		Top,
+		Bottom,
+		Left,
+		Right
+	}
+
+	public class TabPositionToGridRowConverter : MarkupExtension, IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value is TabPosition position)
+			{
+				return position == TabPosition.Bottom ? 1 : 0;
+			}
+			return 0;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public partial class DockingGroup : UserControl
+	{
+		public static readonly DependencyProperty TabPositionProperty =
+			DependencyProperty.Register("TabPosition", typeof(TabPosition), typeof(DockingGroup),
+				new PropertyMetadata(TabPosition.Top));
+
+		public static readonly DependencyProperty TabWidthProperty =
+			DependencyProperty.Register("TabWidth", typeof(double), typeof(DockingGroup),
+				new PropertyMetadata(double.NaN));
+
+		public static readonly DependencyProperty TabHeightProperty =
+			DependencyProperty.Register("TabHeight", typeof(double), typeof(DockingGroup),
+				new PropertyMetadata(double.NaN));
+
+		public static readonly DependencyProperty CloseTabCommandProperty =
+			DependencyProperty.Register("CloseTabCommand", typeof(ICommand), typeof(DockingGroup));
+
+		public TabPosition TabPosition
+		{
+			get { return (TabPosition)GetValue(TabPositionProperty); }
+			set { SetValue(TabPositionProperty, value); }
+		}
+
+		public double TabWidth
+		{
+			get { return (double)GetValue(TabWidthProperty); }
+			set { SetValue(TabWidthProperty, value); }
+		}
+
+		public double TabHeight
+		{
+			get { return (double)GetValue(TabHeightProperty); }
+			set { SetValue(TabHeightProperty, value); }
+		}
+
+		public ICommand CloseTabCommand
+		{
+			get { return (ICommand)GetValue(CloseTabCommandProperty); }
+			set { SetValue(CloseTabCommandProperty, value); }
+		}
+
+		public DockingGroup()
+		{
+			InitializeComponent();
+		}
+	}
+}
