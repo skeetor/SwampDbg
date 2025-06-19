6bbb9d55825956b2ba5b6165edc0295409e524f6
Gerhard Gruber <sparhawk@gmx.at>
2025-06-19 18:35:12 +0200

    Back to the roots

---
+++ WpfDockingManager/DockingPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace WpfDockingManager
+{
+	public class DockingPanel : Grid
+	{
+		public DockingPanel()
+			: base()
+		{
+		}
+		protected override void OnVisualChildrenChanged(DependencyObject visualAdded, DependencyObject visualRemoved)
+		{
+			// Track when objects are added and removed
+			if (visualAdded != null)
+			{
+				// Do stuff with the added object
+			}
+			if (visualRemoved != null)
+			{
+				// Do stuff with the removed object
+			}
+
+			// Call base function
+			base.OnVisualChildrenChanged(visualAdded, visualRemoved);
+		}
+	}
+}
